93dddf396fac047e599aade14c58d19c15620569
jiang_xmin <jiang_xmin@massclouds.com>
2016-04-22 21:07:23 +0800

    add install ipython

--- ubuntu14_dev_init_sh/ubuntu_dev_init.sh
+++ ubuntu14_dev_init_sh/ubuntu_dev_init.sh
@@ -19,6 +19,9 @@ apt-get install qt4-dev-tools qt4-doc qt4-qtconfig qt4-demos qt4-designer -y --f
 apt-get install libxext6 libxext-dev libqt4-dev libqt4-gui libqt4-sql qt4-dev-tools qt4-doc qt4-designer qt4-qtconfig python-qt4-*  python-qt4 -y --force-yes
 apt-get install pyqt4-dev-tools
 
+# install ipython
+apt-get install ipython -y
+
 # install java-8
 
 
