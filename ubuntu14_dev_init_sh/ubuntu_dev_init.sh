5bc93a583b0800c2a7309003084b62f3446a4e0e
jiang_xmin <jiang_xmin@massclouds.com>
2016-05-01 17:54:31 +0800

    添加 install  rar

--- ubuntu14_dev_init_sh/ubuntu_dev_init.sh
+++ ubuntu14_dev_init_sh/ubuntu_dev_init.sh
@@ -14,6 +14,8 @@ apt-get install openssh-server -y --force-yes
 
 #install qt4
 apt-get install qt4-dev-tools qt4-doc qt4-qtconfig qt4-demos qt4-designer -y --force-yes
+#install qpainter
+#apt-get install libqtexengine-dev libqtexengine1 navit-graphics-qt-qpainter  -y
 
 # install pyqt4
 apt-get install libxext6 libxext-dev libqt4-dev libqt4-gui libqt4-sql qt4-dev-tools qt4-doc qt4-designer qt4-qtconfig python-qt4-*  python-qt4 -y --force-yes
@@ -27,4 +29,5 @@ apt-get install ipython -y
 # install svn
 apt-get install subversion
 
-
+# install rar
+apt-get install  rar 
