37498c17365a91d3ca2934463081ead3758ebaa9
jiang_xmin <jiang_xmin@massclouds.com>
2016-05-11 15:48:42 +0800

    mcos_ubuntu14_devInit.sh  +  libva-x11-1  i965-va-driver

--- ubuntu14_dev_init_sh/mcos_ubuntu14_devInit.sh
+++ ubuntu14_dev_init_sh/mcos_ubuntu14_devInit.sh
@@ -6,4 +6,5 @@ apt-get update
 
 # add deb for remote-viewer
 apt-get install vim openssh-server libusbredirparser1 libusbredirhost1 libavcodec-ffmpeg56 libavformat-ffmpeg56 libswscale-ffmpeg3 -y  --force-yes  || exit 155
+sudo apt-get install libva-x11-1   i965-va-driver -y   --force-yes
 echo "=============================================================================================="
