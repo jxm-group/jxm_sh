c66389312f54a3e4c8b3d5ea8ec1f258da4619fc
jiang_xmin <jiang_xmin@massclouds.com>
2016-05-14 19:25:39 +0800

    vim-python-config.sh

---
+++ ubuntu14_dev_init_sh/cmd_sh/bin/help.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo ""
+echo "============== help ==============="
+echo "open pdf  :  evince filename.pdf"
+echo "open image:  eog    filename"
+echo "==================================="
+echo ""
