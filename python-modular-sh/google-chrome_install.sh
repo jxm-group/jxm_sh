f3bd5abb6d059178f9debe94e6764db6fdbea2df
jiang_xmin <jiang_xmin@massclouds.com>
2016-05-21 19:00:11 +0800

    google-chrome_install.sh

---
+++ python-modular-sh/google-chrome_install.sh
@@ -0,0 +1,14 @@
+#svn checkout 
+
+echo "[ info ] svn checkout ............................"
+svn checkout https://github.com/github-jxm/pythonModular.git/trunk/chrome
+cd ./request
+
+echo " "
+echo "[ info ] install chrome............ "
+sudo ./install.sh
+
+cd ../
+rm chrome -rf
+echo "[ info ] -------------install end------------------"
+echo ""
