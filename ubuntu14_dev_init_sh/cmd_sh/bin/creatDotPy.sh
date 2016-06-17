97a7803dad146399675ed43cbba06753a9ef10d4
jiang_xmin <jiang_xmin@masssclouds.com>
2016-06-17 21:21:45 +0800

    ..

--- ubuntu14_dev_init_sh/cmd_sh/bin/creatDotPy.sh
+++ ubuntu14_dev_init_sh/cmd_sh/bin/creatDotPy.sh
@@ -7,20 +7,32 @@ DATETIME=`date -d today +"%Y%m%d%H%M"`
 
 version="v4.5"
 name="Jiangxumin"
-fileName=$1
+fileName=$1.py
+
+if [ "$1" = "" ];then
+    echo "警告：正确用法: creatDotPy fileName"
+    exit 1
+else
+    echo "生成文件: $1.py"
+fi
 
 title="#!/usr/bin/env python\n#coding=utf-8\n"
-info="
-\n
-\n
-\"\"\"
-@version    : $version\n
-@author     : $name\n
-@contact    : jiang_xmin@massclouds.com\n
-@site       : http://blog.csdn.net/jxm_csdn\n
-@file       : $1.py\n
-@time       : $DATETIME\n
-\"\"\"
-"
-echo -e $info > test.log
+echo -e $title > $fileName
 
+echo "\"\"\"" >> $fileName
+echo "@version: $version" >> $fileName
+echo "@author : $name" >> $fileName
+echo "@contact: jiang_xmin@massclouds.com" >> $fileName
+echo "@site   : http://blog.csdn.net/jxm_csdn" >> $fileName
+echo "@file   : $1.py" >> $fileName
+echo "@time   : $DATETIME" >> $fileName
+echo "\"\"\"" >> $fileName >> $fileName
+
+echo -e "\n" >> $fileName
+
+echo -e "def test_main():" >> $fileName 
+echo -e "    pass\n\n" >> $fileName 
+
+echo -e "if __name__ == \"__main__\":" >> $fileName 
+echo -e "    test_main():" >> $fileName 
+echo -e "" >> $fileName
