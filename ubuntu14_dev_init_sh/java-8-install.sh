e3b13530b18fbd21ec9ef0a2a6bfc4fadbb26eb6
jiang_xmin <jiang_xmin@massclouds.com>
2016-08-23 19:00:41 +0800

    ....

--- ubuntu14_dev_init_sh/java-8-install.sh
+++ ubuntu14_dev_init_sh/java-8-install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-#add-apt-repository ppa:webupd8team/java  -y --force-yes
+
+##############################################################################
+#  Problem: apt-get-repository Command is Missing
+#
+#          add-apt-repository: command not found
+# 
+#  Solution: Install the software-properties-common Package 
+#
+#  ----------------------------------------------------------------------------  
+#  $ sudo apt-get install software-properties-common python-software-properties
+#  ----------------------------------------------------------------------------
+#
+###############################################################################
 add-apt-repository ppa:webupd8team/java  -y 
 apt-get update
 apt-get install oracle-java8-installer  -y --force-yes
