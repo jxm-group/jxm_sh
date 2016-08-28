79a2daa6bd4f20897aaeb301c002c99874f68186
jxm_zn <jxm_zn@163.com>
2016-08-28 11:20:00 +0800

    addEnv

---
+++ ubuntu14_dev_init_sh/cmd_sh/addEnv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+bashrc=$HOME/.bashrc
+
+echo -e 'export PATH=/opt/usr/bin\c' >> $bashrc
+echo -e ':$PATH' >> $bashrc 
+
--- ubuntu14_dev_init_sh/cmd_sh/install.sh
+++ ubuntu14_dev_init_sh/cmd_sh/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+usr=`whoami`
+if [ $usr != "root" ];then
+	echo "please use : sudo ./install.sh"
+	exit -1
+fi
 sudo chmod o+rw /opt
 mkdir /opt/usr/bin -p
 cp   ./bin/*  /opt/usr/bin/ -rf
