29702ea6c713277a02110461a67270329b674b59
Jiangxumin <Jiangxumin@users.noreply.github.com>
2016-10-21 16:12:49 +0800

    Update cx_Freeze-4.3.4_install.sh

--- python-modular-sh/cx_Freeze-4.3.4_install.sh
+++ python-modular-sh/cx_Freeze-4.3.4_install.sh
@@ -4,6 +4,8 @@
 # install zlib1g-dev  
 # 编译 cx_Freeze  依赖 
 sudo apt-get install zlib1g-dev
+apt-get install subversion -y --force-yes # SVN
+
 
 echo "[ info ] svn checkout ............................"
 svn checkout https://github.com/github-jxm/pythonModular.git/trunk/cx_Freeze
