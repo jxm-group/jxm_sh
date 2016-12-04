08b27dca3e70dc0fa37ed871aadbc7d5f2624665
jiang_xmin <jiang_xmin@massclouds.com>
2016-12-04 10:27:49 +0800

    ...

--- ubuntu14_dev_init_sh/qemu_kvm_install.sh
+++ ubuntu14_dev_init_sh/qemu_kvm_install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo apt-get install qemu-kvm
-sudo apt-get install bridge-utils
+#sudo apt-get install qemu-kvm
+#sudo apt-get install bridge-utils
+#
+#sudo apt-get install ubuntu-vm-builder
+#sudo apt-get install virt-manager
+#sudo apt-get install qemu-system
 
-sudo apt-get install ubuntu-vm-builder
+
 sudo apt-get install virt-manager
-sudo apt-get install qemu-system
+sudo apt-get install qemu-system-x86
