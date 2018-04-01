93d74d9eb8abfc5d085431a28bb9cb818803f97a
jiangxumin <jxm_zn@163.com>
2018-04-01 09:47:18 +0800

    ....

--- fedora/init.sh
+++
@@ -1,8 +0,0 @@
-sudo yum  install vim
-sudo yum  install golang-github-getlantern-lantern.x86_64
-
-# 安装搜狗输入法 
-# https://www.fdzh.org/blog/2015/10/18/fedora-sougoupinyin/
-
-# yum install google-chrome-stable_current_x86_64.rpm 
-
--- python-modular-sh/pyusb_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 1. 
-sudo pip install pyusb
-
-# 2 . 或者
-
